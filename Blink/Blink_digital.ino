f58d3516c0e30ccfc5a26e7b340b6bc317b86fe5
Mario Alves <mario.alves.souza@outlook.com>
2020-12-08 22:28:24 -0300

    Blink com serial.print

---
+++ Blink/Blink_digital.ino
@@ -0,0 +1,16 @@
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{                    
+  digitalWrite(13, HIGH);
+  Serial.println("Ligado");
+  delay(1000);                     
+  digitalWrite(13, LOW);
+  Serial.println("Desligado");
+  delay(1000);                      
+}
